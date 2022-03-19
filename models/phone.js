54f1750d7420edc569ff4f902a8cb3c97542b6b3
Juan Carlos <jcarlosriego@gmail.com>
2022-03-19 01:32:48 +0100

    Added Phone model and all API routes

---
+++ models/phone.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const dataSchema = new mongoose.Schema({
+    name: {
+        required: true,
+        type: String
+    },
+    manufacturer: {
+        required: true,
+        type: String
+    },
+    description: {
+        required: true,
+        type: String
+    },
+    color: {
+        required: true,
+        type: String
+    },
+    price: {
+        required: true,
+        type: Number
+    },
+    imageFileName: {
+        required: true,
+        type: String
+    },
+    screen: {
+        required: true,
+        type: String
+    },
+    processor: {
+        required: true,
+        type: String
+    },
+    ram: {
+        required: true,
+        type: Number
+    },
+    
+})
+
+module.exports = mongoose.model('Phone', dataSchema)
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,67 @@
-const express = require('express');
+const express = require('express')
+const Phone = require('../models/phone')
+const router = express.Router()
 
-const router = express.Router();
+module.exports = router
 
-module.exports = router;
+router.post('/phones', async (req, res) => {
+    const data = new Phone({
+        name: req.body.name,
+        manufacturer: req.body.manufacturer,
+        description: req.body.description,
+        color: req.body.color,
+        price: req.body.price,
+        imageFileName: req.body.imageFileName,
+        screen: req.body.screen,
+        processor: req.body.processor,
+        ram: req.body.ram
+    })
+
+    try{
+        const dataToSave = await data.save()
+        res.status(200).json(dataToSave)
+    }
+    catch(error){
+        res.status(400).json({message: error.message})
+    }
+})
+
+
+router.get('/phones', async (req, res) => {
+    try{
+        const data = await Phone.find()
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+router.patch('/phones/:id', async (req, res) => {
+    try{
+        const id = req.params.id
+        const updatedData = req.body
+        const options = { new: true }
+
+        const result = await Phone.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch(error){
+        res.status(400).json({ message: error.message })
+    }
+})
+
+router.delete('/phones/:id', async (req, res) => {
+    try{
+        const id = req.params.id
+        const data = await Phone.findByIdAndDelete(id)
+        
+        res.send(`${data.name} has been deleted...`)
+    }
+    catch(error){
+        res.status(400).json({ message: error.message })
+    }
+})
