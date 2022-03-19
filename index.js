3d6bcc02093f524b643a116bef41dfd9f513577e
Juan Carlos <jcarlosriego@gmail.com>
2022-03-19 18:49:11 +0100

    .env removed from gitignore for review purposes and added Cors setup

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes/routes')
+const cors = require('cors')
+
 let mongoString = process.env.NODE_ENV === 'testing' 
     ? process.env.DATABASE_URI_TEST
     : process.env.DATABASE_URI
@@ -15,9 +17,27 @@ const database = mongoose.connection
 database.on('error', (error) => console.log(error))
 database.once('connected', () => console.log('Database Connected'))
 
+const whitelist = [
+    'http://localhost:3000',
+]
+
+const corsOptions = {
+    origin: function(origin, callback){
+        if(!origin) return callback(null, true);
+        if(whitelist.indexOf(origin) === -1){
+          var msg = `The CORS policy for this site does not 
+                    allow acces from the specified Origin.`
+          return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+      },
+    credentials: true
+}
+
 const app = express()
 
 app.use(express.json())
+app.use(cors(corsOptions))
 app.use('/api', routes)
 app.listen(8000, () => {
     console.log(`Server Started at ${8000}`)
